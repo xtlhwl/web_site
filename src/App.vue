0ad0b8092625538ced30a294b90bf5e4d0a033d3
Seven <xtlhcy@gamil.com>
2020-06-19 11:03:19 +0800

    提交2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="1Welcome to Your Vue.js App"/>
+    <HelloWorld msg="12Welcome to Your Vue.js App"/>
   </div>
 </template>
 
